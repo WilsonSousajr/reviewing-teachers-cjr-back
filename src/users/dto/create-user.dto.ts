b303024e96f9f4ee84605c5a6014db7e2f46c607
Caselli <11felipe.caselli@gmail.com>
2024-05-24 18:03:33 -0300

    🚧✨ New feature: create user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,27 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsInt,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsInt()
+  id: number;
+  @IsEmail()
+  email: string;
+  @IsNotEmpty({ message: 'Precisa de Senha!!' })
+  @IsStrongPassword()
+  password: string;
+  @IsString()
+  name: string;
+  @IsString()
+  @IsOptional()
+  picture: string;
+  @IsString()
+  departament: string;
+  @IsString()
+  course: string;
+}
