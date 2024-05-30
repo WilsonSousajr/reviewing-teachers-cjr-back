2060de891df8026ed20b3a5e7d236d8b8a0c217c
Felipe <11felipe.caselli@gmail.com>
2024-05-30 10:12:35 -0300

    🎉 Start: start teachers CRUD

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TeachersModule } from './teachers/teachers.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TeachersModule],
   controllers: [AppController],
   providers: [AppService],
 })
