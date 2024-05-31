ddae0436bce84c9d280485c9ffc6b722651872b9
Wilson Sousa <wstechnology.br@gmail.com>
2024-05-31 11:36:13 -0300

    ✨ Add: Create reviews.service.ts

---
+++ src/reviews/reviews.service.ts
@@ -0,0 +1,61 @@
+import { Injectable } from '@nestjs/common';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { UpdateReviewDto } from './dto/update-review.dto';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class ReviewsService {
+  constructor(private readonly prisma: PrismaService) {}
+  async create(createReviewDto: CreateReviewDto) {
+    return await this.prisma.review.create({
+      data: {
+        user: {
+          connect: {
+            id: createReviewDto.userId,
+          },
+        },
+        teacher: {
+          connect: {
+            id: createReviewDto.teacherId,
+          },
+        },
+        discipline: {
+          connect: {
+            id: createReviewDto.disciplineId,
+          },
+        },
+        title: createReviewDto.title,
+        content: createReviewDto.content,
+      },
+    });
+  }
+
+  async findAll() {
+    return await this.prisma.review.findMany();
+  }
+
+  async findOne(id: number) {
+    return await this.prisma.review.findMany({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: number, updateReviewDto: UpdateReviewDto) {
+    return await this.prisma.review.update({
+      where: {
+        id,
+      },
+      data: updateReviewDto,
+    });
+  }
+
+  async remove(id: number) {
+    return await this.prisma.review.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
