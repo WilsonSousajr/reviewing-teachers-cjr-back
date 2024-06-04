fc6aa279cb8a1f4275adfb274645bb2cf315346b
Caselli <11felipe.caselli@gmail.com>
2024-06-04 20:19:43 -0300

    🚑 HotFix: cors = true

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {cors:true});
+  await app.listen(3333);
 }
 bootstrap();
