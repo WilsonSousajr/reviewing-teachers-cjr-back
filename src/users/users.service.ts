8f4b0d9cd7833141a16c24762e056c9639c64a0f
Wilson Sousa <wstechnology.br@gmail.com>
2024-06-04 21:32:30 -0300

    🚧 (WIP): Start to create hashing password logic

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/prisma.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prisma: PrismaService) {}
 
   async create(createUserDto: CreateUserDto) {
+    const existingUser = await this.prisma.user.findUnique({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+
+    if (existingUser){
+      throw new ConflictException('Email already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(createUserDto.password);
+
     return await this.prisma.user.create({data: createUserDto})
   }
 
