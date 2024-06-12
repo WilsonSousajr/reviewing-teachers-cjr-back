d5b48c6cf050e3d439706567e2dc74a1c5f1a1ae
Wilson Sousa <wstechnology.br@gmail.com>
2024-06-12 00:29:27 -0300

    ✨ Add: Create a saving pictures logic in users.controller.ts

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Express } from 'express'; 
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  @UseInterceptors(FileInterceptor('picture', {
+    storage: diskStorage({
+      destination: './uploads', 
+      filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, `${uniqueSuffix}${extname(file.originalname)}`);
+      },
+    }),
+  }))
+  create(@Body() createUserDto: CreateUserDto, @UploadedFile() file: Express.Multer.File) {
+    if (file) {
+      createUserDto.picture = file.filename;
+    }
     return this.usersService.create(createUserDto);
   }
 
