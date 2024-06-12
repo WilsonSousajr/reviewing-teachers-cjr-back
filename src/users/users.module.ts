0cae69d7122459f5f3b048b254d2825566ed6878
Wilson Sousa <wstechnology.br@gmail.com>
2024-06-12 13:22:52 -0300

    ✨ Add: Export UsersService

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -8,5 +8,6 @@ import { PrismaService } from 'src/prisma.service';
 @Module({
   controllers: [UsersController],
   providers: [UsersService, PrismaService],
+  exports: [UsersService],
 })
 export class UsersModule {}
