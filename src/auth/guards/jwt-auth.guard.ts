742b1a342659932c84649312ac46b42ffeba85aa
Wilson Sousa <wstechnology.br@gmail.com>
2024-06-16 02:16:22 -0300

    🔒️ Add: Update jwt-auth.guard

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,5 +1,18 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Injectable, ExecutionContext, UnauthorizedException } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
+import { Observable } from "rxjs";
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt'){
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        return super.canActivate(context);
+    }
+
+    handleRequest(err, user, info){
+        if (err || !user){
+            throw err || new UnauthorizedException();
+        }
+
+        return user;
+    }
+}
