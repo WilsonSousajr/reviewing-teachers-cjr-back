16a3ff0cf1c93f81b6a3b510dce62063486df35b
Wilson Sousa <wstechnology.br@gmail.com>
2024-06-16 02:18:52 -0300

    🔒️ Add: Improve teachers.controller security

--- src/teachers/teachers.controller.ts
+++ src/teachers/teachers.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { TeachersService } from './teachers.service';
 import { CreateTeacherDto } from './dto/create-teacher.dto';
 import { UpdateTeacherDto } from './dto/update-teacher.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('teachers')
 export class TeachersController {
   constructor(private readonly teachersService: TeachersService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createTeacherDto: CreateTeacherDto) {
     return this.teachersService.create(createTeacherDto);
@@ -17,8 +19,8 @@ export class TeachersController {
     return this.teachersService.findAll();
   }
 
-  @Get('name:name')
-  findByName(@Param('name') name: string){
+  @Get('name/:name')
+  findByName(@Param('name') name: string) {
     return this.teachersService.findByName(name);
   }
   
@@ -27,11 +29,13 @@ export class TeachersController {
     return this.teachersService.findOne(+id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateTeacherDto: UpdateTeacherDto) {
     return this.teachersService.update(+id, updateTeacherDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.teachersService.remove(+id);
